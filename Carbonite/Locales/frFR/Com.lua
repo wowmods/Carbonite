a0a4136bf28423740e6df27067e69d0667c3de48
Olivier <powerstrk@outlook.be>
2014-11-12 09:03:00 +0100

    Update Com.lua
    Carbonite is on track... File (s) adjust the constraints of programming characters
    Special French language are very common ... Insert code to replace
    the é è ç ê ......... This will make me crazy :-)
    I hope the final file, awaiting new data WOD, or other missing data?
    I hope to see local files running one day with a tool designed for ?
    THX Rythal, have a nice day.

--- Carbonite/Locales/frFR/Com.lua
+++ Carbonite/Locales/frFR/Com.lua
@@ -6,20 +6,20 @@ local L = LibStub("AceLocale-3.0"):NewLocale("Carbonite", "frFR", true)
 if not L then return end
 
 L["reached level"] = "Niveau Atteint"
-L["Monitor Error: All 10 chat channels are in use"] = "Monitor Erreur : les 10 canaux du chat sont utilisés"
-L["This will disable some communication features"] = "Ceci désactivera certaines fonctions de communication"
-L["You may free channels using the chat tab"] = "Vous pouvez libérer les canaux en utilisant l'onglet du chat"
-L["chat channel(s)!"] = "Canaux de discussion (s)"
+L["Monitor Error: All 10 chat channels are in use"] = "Monitor Erreur: les 10 canaux du chat sont utilis\195\169s"
+L["This will disable some communication features"] = "Ceci d\195\169sactivera certaines fonctions de communication"
+L["You may free channels using the chat tab"] = "Vous pouvez lib\195\169rer les canaux en utilisant l'onglet du chat"
+L["chat channel(s)!"] = "Canaux de discussion (s)!"
 L["Need"] = "Besoin"
-L["Monitored:"] = "Modérateur"
+L["Monitored:"] = "Mod\195\169rateur"
 L["Druid"] = "Druide"
 L["Hunter"] = "Chasseur"
 L["Mage"] = true
 L["Paladin"] = true
-L["Priest"] = "Prêtre"
+L["Priest"] = "Pr\195\170tre"
 L["Rogue"] = "Voleur"
 L["Shaman"] = true
-L["Warlock"] = "Démoniste"
+L["Warlock"] = "D\195\169moniste"
 L["Warrior"] = "Guerrier"
 L["Deathknight"] = "Chevalier de la mort"
 L["Monk"] = "Moine"
