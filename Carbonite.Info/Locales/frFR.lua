c583a0d4467a9b06d313dd95ee28e82af29eb15d
Olivier <powerstrk@outlook.be>
2014-11-12 16:56:18 +0100

    Update frFR.lua
    File (s) adjust the constraints of programming characters
    Special French language are very common ... Insert code to replace
    the é è ç ê .

--- Carbonite.Info/Locales/frFR.lua
+++ Carbonite.Info/Locales/frFR.lua
@@ -6,34 +6,34 @@ local L = LibStub("AceLocale-3.0"):NewLocale("Carbonite.Info", "frFR", true)
 if not L then return end
 
 L["Info Options"] = true
-L["Lock Info Windows"] = "Verrouiller Fenêtre Info"
-L["Locks the location of your info windows"] = "Verrouiller l'emplacement de la fenêtre Info"
-L["Info Window Background Color"] = "Couleur de fond de la fenêtre Info"
-L["Info Font"] = "Police de la fenêtre Info"
-L["Sets the font to be used for info windows"] = "Définir la police de la fenêtre Info"
+L["Lock Info Windows"] = "Verrouiller Fen\195\170tre Info"
+L["Locks the location of your info windows"] = "Verrouiller l'emplacement de la fen\195\170tre Info"
+L["Info Window Background Color"] = "Couleur de fond de la fen\195\170tre Info"
+L["Info Font"] = "Police de la fen\195\170tre Info"
+L["Sets the font to be used for info windows"] = "D\195\169finir la police de la fen\195\170tre Info"
 L["Info Font Size"] = "Taille de la police Info"
-L["Sets the size of the info font"] = "Définir la taille de police de la fenêtre Info"
-L["Info Font Spacing"] = "Espacement de la police fenêtre Info"
-L["Sets the spacing of the info font"] = "Définir l'espacement de la police fenêtre Info"
-L["Show Info Windows"] = "Afficher fenêtre Info"
+L["Sets the size of the info font"] = "D\195\169finir la taille de police de la fen\195\170tre Info"
+L["Info Font Spacing"] = "Espacement de la police fen\195\170tre Info"
+L["Sets the spacing of the info font"] = "D\195\169finir l'espacement de la police fen\195\170tre Info"
+L["Show Info Windows"] = "Afficher fen\195\170tre Info"
 L["Toggle Info Windows"] = true
 L["Info Module"] = true
 L["Close"] = "Fermer"
 L["Edit Item"] = "Editer Objet"
 L["Show"] = "Afficher"
-L["New Info Window"] = "Nouvelle fenêtre Info"
-L["Delete This Window"] = "Effacer cette fenêtre"
+L["New Info Window"] = "Nouvelle fen\195\170tre Info"
+L["Delete This Window"] = "Effacer cette fen\195\170tre"
 L["Options"] = true
 L["Info"] = true
 L["Edit View"] = "Editer la Vue"
-L["Stop Edit"] = "Stopper l'édition"
+L["Stop Edit"] = "Stopper l'\195\169dition"
 L["Change Text"] = "Changer le Texte"
-L["Delete Info Window"] = "Effacer fenêtre Info"
+L["Delete Info Window"] = "Effacer fen\195\170tre Info"
 L["Delete"] = "Effacer"
 L["Cancel"] = "Annuler"
 
-L["One minute until the Arena"] = "Une Minute avant l'arène"
-L["Thirty seconds until the Arena"] = "Trente Secondes avant l'arène"
-L["Fifteen seconds until the Arena"] = "Quinze Secondes avant l'arène"
+L["One minute until the Arena"] = "Une Minute avant l'ar\195\168ne"
+L["Thirty seconds until the Arena"] = "Trente Secondes avant l'ar\195\168ne"
+L["Fifteen seconds until the Arena"] = "Quinze Secondes avant l'ar\195\168ne"
 
 
