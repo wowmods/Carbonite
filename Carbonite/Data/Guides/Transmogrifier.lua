a72ea9afe2161ee22c73211b919a2ba52b030498
nelegalno <nelegalno@yopmail.com>
2014-11-27 15:02:00 +0200

    more white space cleaning

--- Carbonite/Data/Guides/Transmogrifier.lua
+++ Carbonite/Data/Guides/Transmogrifier.lua
@@ -1,7 +1,7 @@
 local L = LibStub("AceLocale-3.0"):GetLocale("Carbonite")
 
 Nx.GuideData[L["Transmogrifier"]] = {
-     ["Mode"] = 32,
-     [301] = "1,50.8,60.8",
-     [321] = "2,58,65.4",
+	["Mode"] = 32,
+	[301] = "1,50.8,60.8",
+	[321] = "2,58,65.4",
 }
