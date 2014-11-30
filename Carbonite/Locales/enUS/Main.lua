e0c99bbbdd4b695c5bb817ee21ce7f2791a1bc2c
Alex Kloss <alexthkloss@web.de>
2014-11-30 12:08:54 +0100

    Localize UI Tooltips

--- Carbonite/Locales/enUS/Main.lua
+++ Carbonite/Locales/enUS/Main.lua
@@ -108,6 +108,16 @@ L["Scale"] = true
 L["Transparency"] = true
 L["Reset Layout"] = true
 
+-- UI Tooltips
+L["Close/Menu"] = true
+L["Close/Unlock"] = true
+L["Pick Color"] = true
+L["Unlock"] = true
+L["Maximize"] = true
+L["Restore"] = true
+L["Minimize"] = true
+L["Auto Scale"] = true
+
 -- Stuff from old localization
 L["Searching for Artifacts"] = true			-- NXlARTIFACTS
 L["Extract Gas"] = true						-- NXlEXTRACTGAS
