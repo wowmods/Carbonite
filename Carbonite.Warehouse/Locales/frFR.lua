9e41675f8c93773abc3e5afadf7e409422a1f5ac
Lars Christian <admin@wowfriends.de>
2014-12-03 02:27:33 +0100

    control failures II

--- Carbonite.Warehouse/Locales/frFR.lua
+++ Carbonite.Warehouse/Locales/frFR.lua
@@ -5,7 +5,6 @@ end
 local L = LibStub("AceLocale-3.0"):NewLocale("Carbonite.Warehouse", "frFR")
 if not L then return end
 
-<<<<<<< HEAD
 L["ItemTypes"] = {
 	ARMOR,
 	"Consommable",
@@ -23,29 +22,6 @@ L["ItemTypes"] = {
 }
 
 L["Warehouse Module"] = true
-L["Warehouse Options"] = true
-L["Add Warehouse Tooltip"] = true
-L["When enabled, will show warehouse information in hover tooltips of items"] = true
-L["Warehouse Font"] = true
-L["Sets the font to be used for warehouse windows"] = true
-L["Warehouse Font Size"] = true
-L["Sets the size of the warehouse font"] = true
-L["Warehouse Font Spacing"] = true
-L["Sets the spacing of the warehouse font"] = true
-L["Toggle Warehouse"] = true
-L["Remove Character or Guild"] = true
-L["Import settings from selected character"] = true
-L["Export current settings to all characters"] = true
-L["Sync account transfer file"] = true
-L["Show Lowest Equipped Rarity"] = true
-L["Show Item Headers"] = true
-L["Sort By Rarity"] = true
-L["Show Lowest Rarity"] = true
-L["Sort By Slot"] = true
-L["Import %s's character data and reload?"] = true
-L["Overwrite all character settings and reload?"] = true
-L["Warehouse: %d characters"] = true
-=======
 L["Warehouse Options"] = "Options Inventaires/D\195\169p\195\180ts"
 L["Add Warehouse Tooltip"] = "Ajouter infobulle inventaires/d\195\169p\195\180ts"
 L["When enabled, will show warehouse information in hover tooltips of items"] = "Quand activ\195\169, affiche l'information des objets que vous avez dans l'inventaire/d\195\169p\195\180ts"
@@ -68,5 +44,4 @@ L["Sort By Slot"] = "Trier par emplacement"
 L["Import %s's character data and reload?"] = "Importer %s's donn\195\169es du personnage et red\195\169marrer?"
 L["Overwrite all character settings and reload?"] = "Ecraser tous les param\195\168tres des personnages et red\195\169marrer?"
 L["Warehouse: %d characters"] = "Inventaires/D\195\169p\195\180ts %d personnages"
->>>>>>> 36231b4403244107aacfc2d034e06cdb5dadb280
 L["DurPattern"] = "^Durabilit\195\169 (%d+) / (%d+)"
